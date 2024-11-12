ed5e7ddc56b1ffa37ed3025c861f1172e90c57e5
estéfani. <128320162+estyprimo@users.noreply.github.com>
2024-11-12 16:55:08 -0300

    Create 1007.cpp

---
+++ 1007.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a, b, c, d;
+
+    cin >> a >> b >> c >> d; // recebe os valores
+
+    int dif = ((a*b) - (c*d)); // faz a equação necessária
+
+    cout << "DIFERENCA = " << dif << endl; // imprime o resultado
+    
+    return 0;
+}
